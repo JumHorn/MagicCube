205f04f1e654cf5cd9eef4529e40b3237a158d38
JumHorn <JumHorn@gmail.com>
2018-10-24 00:02:15 +0800

    add perspective resize model

--- cube.cpp
+++ cube.cpp
@@ -10,13 +10,26 @@ Cube::~Cube()
 void Cube::initializeGL()
 {
 	glClearColor(0.0f,0.0f,0.0f,0.0f);
+	glColor3f(1.0f,0.0f,0.0f);
 }
 
 void Cube::resizeGL(int width,int height)
-{}
+{
+	if(height==0)
+		height=1;
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glViewport(0,0,width,height);
+	gluPerspective(45.0f,(GLfloat)width/(GLfloat)height,0.1f,100.0f);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
 
 void Cube::paintGL()
-{}
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+}
 
 //global varible
 Cube c;
